9d6cc3c0097c4dbfe2194812799c3c8c06f37023
Marek Janik <marekadamjanik@gmail.com>
2022-10-05 14:56:15 +0200

    Create search bar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react';
 
+import { SearchBar } from '../search-bar';
+
 import styles from './header.module.scss';
 
 type HeaderProps = {};
 
 export const Header: FC<HeaderProps> = () => {
-  return <header className={styles.header}></header>;
+  return (
+    <header className={styles.header}>
+      <SearchBar />
+    </header>
+  );
 };
--- src/components/search-bar/search-bar.tsx
+++ src/components/search-bar/search-bar.tsx
@@ -1,5 +1,19 @@
 import { FC } from 'react';
 
+import styles from './search-bar.module.scss';
+
 export const SearchBar: FC = () => {
-  return <div></div>;
+  return (
+    <form className={styles.search} role="search" action="/" method="get">
+      <label className={styles.search__label} htmlFor="search">
+        Podaj NIP lub nazwę dłużnika
+      </label>
+
+      <input className={styles.search__input} type="text" id="search" />
+
+      <button className={styles.search__button} type="submit">
+        Szukaj
+      </button>
+    </form>
+  );
 };
